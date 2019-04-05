31a164a575f0c7f3cf72d250eeaf677696135aa2
Elise <robotbrainify@gmail.com>
2019-04-05 11:44:01 -0400

    Load and MPI Init

--- main.c
+++ main.c
@@ -4,13 +4,20 @@
 #define DATA_LENGTH 1 << 27
 
 int arraylen;
-long long* array;    // length arraylen
-long long* scratch;  // length 2 * arraylen
+long long *array;    // length arraylen
+long long *scratch;  // length 2 * arraylen
 int mpi_rank, mpi_size;
 
 void merge(void);
 
+void load(char *fpath);
+
 int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
     return 0;
 }
 
@@ -20,4 +27,11 @@ int main(int argc, char **argv) {
  */
 void merge(void) {
     return;
+}
+
+void load(char *fpath) {
+    MPI_File fh;
+    MPI_File_open(MPI_COMM_WORLD, fpath, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+    MPI_File_read_at_all(fh, mpi_rank * arraylen, array, arraylen, MPI_LONG_LONG_INT, MPI_STATUS_IGNORE);
+    MPI_File_close(&fh);
 }
