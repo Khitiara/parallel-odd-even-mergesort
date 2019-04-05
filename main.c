8535a5f6c5b9d24ca17378020d3f7c29cef6caa9
Chris Nero <chrisvn00@gmail.com>
2019-04-05 11:38:27 -0400

    Declare globals

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <mpi.h>
 
+int arraylen;
+long long* array;    // length arraylen
+long long* scratch;  // length 2 * arraylen
+
+void merge(void);
+
 int main(int argc, char** argv)
 {
     return 0;
+}
+
+/**
+ * Merges two arrays stored in the lower and upper halves of scratch
+ * into array.
+ */
+void merge(void) {
+    return;
 }
